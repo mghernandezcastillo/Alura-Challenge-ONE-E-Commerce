00083b1fb470aa2e34378532422e4f17e8e0aefb
Michael Hernández <mghernandezcastillo@gmail.com>
2022-04-28 13:02:28 -0500

    fix bug home

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 // redirect to home page
 function redirectToHome() {
-  let root = window.location.origin;
-  window.location.replace(`${root}/screens/home.html`);
+  window.location.href = "screens/home.html";
 }
 
 redirectToHome();
